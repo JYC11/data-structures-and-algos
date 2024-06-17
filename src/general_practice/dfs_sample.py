1889f6606818625060d2065290e45b93143b0c96
JYC11 <jaeyoon.c@gmail.com>
2024-06-17 16:57:01 +0900

    dfs with stack

--- src/general_practice/dfs_sample.py
+++ src/general_practice/dfs_sample.py
@@ -9,3 +9,16 @@ def dfs(graph, v, visited):
     print(v, end="")
     for i in graph[v]:
         dfs(graph, i, visited)
+
+
+def depth_first_search(graph, start):
+    stack = [start]
+    visited = set()
+    while stack:
+        vertex = stack.pop()
+        if vertex in visited:
+            continue
+        yield vertex
+        visited.add(vertex)
+        for neighbor in graph[vertex]:
+            stack.append(neighbor)
